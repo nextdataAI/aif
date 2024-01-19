cf43fd8ec24b6628e7a8b9493c81b7cc69279166
StefanoZanoni <stefano.zanoni00@gmail.com>
2024-01-19 12:23:23 +0100

    Remove MyRewardManager from Algorithm class
    The MyRewardManager class and its associated methods have been removed from the Algorithm class. Instead of creating a custom reward manager, the default one from gym.make is now used, which simplifies the algorithm setup. The max_episode_steps parameter has been increased to 1000, allowing longer run times per episode.

--- algorithms/Algorithm.py
+++ algorithms/Algorithm.py
@@ -11,32 +11,13 @@ __all__ = ['Algorithm']
 from minihack import RewardManager, MiniHack
 
 
-class MyRewardManager(RewardManager):
-    def __init__(self):
-        super().__init__()
-        self.memory = []
-        self.previous_distance = 0
-        self.add_custom_reward_fn(self.reward_fn)
-
-    def reward_fn(self, env: MiniHack, state: Any, action: int, new_state: Any):
-        player_pos = get_player_location(state[1])
-        if player_pos not in self.memory:
-            self.memory.append(player_pos)
-        if manhattan_distance(player_pos, get_target_location(state[1])) < self.previous_distance:
-            return 0.5
-        if player_pos == get_target_location(state[1]):
-            return 1
-        else:
-            return -0.01
-
-
 class Algorithm:
     def __init__(self, env_name: str, name: str = "Algorithm"):
         self.stop = None
         self.start = None
-        reward_manager = MyRewardManager()
-        self.env = gym.make(env_name, observation_keys=("chars", "pixel"), reward_manager=reward_manager,
-                            max_episode_steps=100)
+        self.env = gym.make(env_name,
+                            observation_keys=("chars", "pixel"),
+                            max_episode_steps=1000)
         self.name = name
 
     def initialize_env(self, seed: int, informed: bool = True):
