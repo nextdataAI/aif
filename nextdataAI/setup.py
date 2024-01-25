e4540d9bbea96731fbdc91c99fd850f31c64fb40
Paul Magos <p.magos@studenti.unipi.it>
2024-01-25 20:48:12 +0100

    Just refactor

--- nextdataAI/setup.py
+++ nextdataAI/setup.py
@@ -1,22 +1,21 @@
 from setuptools import find_packages, setup
 
-README = open("README.md").read()
+README = open("../README.md").read()
 
 setup(
     name='nextdataAI',
     packages=find_packages(),
-    version='0.0.2',
+    version='0.0.1',
     long_description_content_type="text/markdown",
     url="https://github.com/nextdataAI/aif",
     long_description=README,
-    author_email='chuckpaul98@icloud.com',
-    install_requires=['numpy', 'matplotlib', 'pandas', 'tqdm', 'wandb'],
+    author_email='chuckpaul98@icloud.com, s.zanoni@studenti.unipi.it',
+    install_requires=['wandb', 'numpy', 'matplotlib', 'scipy','gym','nethack','minihack','nle','keras','tensorflow','pytorch','Pillow','imageio','pandas'],
     setup_requires=['pytest-runner'],
     author='Paul Magos & Stefano Zanoni',
     python_requires='>=3.9',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
 )
